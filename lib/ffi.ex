f1a8e8cace548015ce61ed445a8ae069bd12ee8c
Josh Nussbaum <joshnuss@gmail.com>
2015-09-20 21:15:55 -0400

    Added functions to FFI module

--- lib/ffi.ex
+++ lib/ffi.ex
@@ -1,2 +1,14 @@
 defmodule FFI do
+  @on_load :init
+
+  def init do
+    :erlang.load_nif("./ffi_nif", 0)
+  end
+
+  def call({library, name, arguments, return_type}, params),
+    do: nif_call(library, name, arguments, return_type, params)
+
+  def nif_call(library, name, arguments, return_type, params) do
+    :badarg
+  end
 end
